6a709e9fc7c597959c82015f602f91e1ebfc8a62
Kyran Sciberras <kyranu2@gmail.com>
2026-09-13 13:17:53 +0100

    fix security issues

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -30,9 +32,13 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+	portNum, err := strconv.Atoi(portStr)
+	if err != nil || portNum <= 0 || portNum > 65535 {
+		log.Fatal("PORT must be a valid TCP port number")
 	}
 
 	apiCfg := apiConfig{}
@@ -89,10 +95,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + strconv.Itoa(portNum),
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %d\n", portNum)
 	log.Fatal(srv.ListenAndServe())
 }
